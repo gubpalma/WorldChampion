585981d67e2a1b97ec6292483aac5547deccd960
Gabriel Palma <gabriel.palma@sportsbet.com.au>
2017-05-22 14:48:39 +1000

    Added number selection functionality, saving, corrected errors, wipe data

--- src/js/io.js
+++ src/js/io.js
@@ -1,4 +1,4 @@
-var json_program_string =  '{"days":{"Day 1 (Arms)":[{"Id":1,"Name":"Close-Grip Barbell Bench Press","History":[]},{"Id":2,"Name":"Close-Grip Pulldown","History":[]},{"Id":3,"Name":"Lying Triceps Press (Skull Crusher)","History":[]},{"Id":4,"Name":"Dumbbell Alternate Bicep Curl","History":[]},{"Id":5,"Name":"Dips","History":[]}],"Day 2 (Back)":[{"Id":6,"Name":"Pullups","History":[]},{"Id":7,"Name":"Deadlift","History":[]},{"Id":8,"Name":"Reverse Grip Bent-Over Rows","History":[]},{"Id":9,"Name":"Wide-Grip Lat Pulldown","History":[]},{"Id":10,"Name":"One-Arm Dumbbell Row","History":[]}],"Day 3 (Legs)":[{"Id":11,"Name":"Barbell Squat","History":[]},{"Id":12,"Name":"Leg Press","History":[]},{"Id":13,"Name":"Seated Leg Curl","History":[]},{"Id":14,"Name":"Leg Extensions","History":[]},{"Id":15,"Name":"Calf Raises","History":[]}],"Day 4 (Chest)":[{"Name":"Barbell Incline Bench Press Medium-Grip","History":[]},{"Id":16,"Name":"Dumbbell Bench Press","History":[]},{"Id":17,"Name":"Dumbbell Flys","History":[]},{"Id":18,"Name":"Bent-Arm Dumbbell Pullover","History":[]}],"Day 5 (Shoulders)":[{"Id":19,"Name":"Push Press","History":[]},{"Id":20,"Name":"Upright Barbell Row","History":[]},{"Id":21,"Name":"One-Arm Incline Lateral Raise","History":[]},{"Id":22,"Name":"Front Dumbbell Raise","History":[]},{"Id":23,"Name":"Bent Over Low-Pulley Side Lateral","History":[]}]}}';
+var json_program_string = '{"days":{"Day 1 (Arms)":[{"Id":1,"Name":"Close-Grip Barbell Bench Press"},{"Id":2,"Name":"Close-Grip Pulldown"},{"Id":3,"Name":"Lying Triceps Press (Skull Crusher)"},{"Id":4,"Name":"Dumbbell Alternate Bicep Curl"},{"Id":5,"Name":"Dips"}],"Day 2 (Back)":[{"Id":6,"Name":"Pullups"},{"Id":7,"Name":"Deadlift"},{"Id":8,"Name":"Reverse Grip Bent-Over Rows"},{"Id":9,"Name":"Wide-Grip Lat Pulldown"},{"Id":10,"Name":"One-Arm Dumbbell Row"}],"Day 3 (Legs)":[{"Id":11,"Name":"Barbell Squat"},{"Id":12,"Name":"Leg Press"},{"Id":13,"Name":"Seated Leg Curl"},{"Id":14,"Name":"Leg Extensions"},{"Id":15,"Name":"Calf Raises"}],"Day 4 (Chest)":[{"Id":16,"Name":"Barbell Incline Bench Press Medium-Grip"},{"Id":17,"Name":"Dumbbell Bench Press"},{"Id":18,"Name":"Dumbbell Flys"},{"Id":19,"Name":"Bent-Arm Dumbbell Pullover"}],"Day 5 (Shoulders)":[{"Id":20,"Name":"Push Press"},{"Id":21,"Name":"Upright Barbell Row"},{"Id":22,"Name":"One-Arm Incline Lateral Raise"},{"Id":23,"Name":"Front Dumbbell Raise"},{"Id":24,"Name":"Bent Over Low-Pulley Side Lateral"}]}}';
 
 var json_program_array = [];
 
@@ -6,11 +6,11 @@ var documentsDir;
 
 var workoutFile;
 
-function loadorCreateDataFile() {	
+function loadOrCreateDataFile() {
 	tizen.filesystem.resolve('documents', function(dir) {
 		documentsDir = dir;
 		dir.listFiles(onsuccess, onerror);
-	}, onerror, "rw");
+	}, onError, "rw");
 }
 
 function onsuccess(files) {
@@ -26,40 +26,57 @@ function onsuccess(files) {
 
 	if (!fileLoaded) {
 		console.log("File not found, creating...");
-		workoutFile = documentsDir.createFile("workoutData.json");	
+		workoutFile = documentsDir.createFile("workoutData.json");
 		json_program_array = JSON.parse(json_program_string);
 		saveToFile();
 	}
-	
+
 	workoutFile.readAsText(function(str) {
 		json_program_array = JSON.parse(str);
 		console.log("the file = " + workoutFile);
 		console.log("the file, parsed = " + json_program_array);
 		loadDays();
-	}, onerror, "UTF-8");	
-		
-	/*var history = { weight: 40, reps: 8, sets: 2, date: Date.now() };
-	var strrr = JSON.stringify(history);
-	console.log(strrr);*/
-	
+	}, onError, "UTF-8");
 }
 
 function saveToFile() {
-	
+
 	var jsonString = JSON.stringify(json_program_array);
-	
+
 	console.log("the file, stringified = " + jsonString);
-	
+
 	workoutFile.openStream("w", function(fs) {
 		fs.write(jsonString);
 		fs.position = 0;
 		fs.read(fs.fileSize);
 		fs.close();
 		console.log("writing to file");
-	}, onerror, "UTF-8");
+	}, onError, "UTF-8");
 }
 
-function onerror(error) {
+function loadThenDeleteDataFile() {
+	tizen.filesystem.resolve('documents', function(dir) {
+		dir.listFiles(function(files) {
+			files
+					.forEach(function(file) {
+						if (!file.isDirectory) {
+							if (file.name === "workoutData.json") {
+								console.log("File found, deleting...");
+								dir.deleteFile(file.fullPath, onDeleteSuccess,
+										onError);
+							}
+						}
+					})
+		});
+	});
+}
+
+function onDeleteSuccess() {
+	var wipeToast = document.getElementById("wipeToast");
+	tau.openPopup(wipeToast);
+	loadOrCreateDataFile();
+}
+
+function onError(error) {
 	console.log("Error: " + error.message);
 }
-
--- src/js/tau.js
+++ src/js/tau.js
@@ -4,90 +4,95 @@ var currentExercise = [];
 var currentProgram = [];
 var currentHistory = [];
 
+var currentWeightValue = 0;
+var currentRepsValue = 0;
+var currentSetsValue = 0;
+
 var vList;
 
 (function(tau) {
-	window.addEventListener("pageshow", function (e) {
+	window.addEventListener("pageshow", function(e) {
 
-	page = e.target;
-		
-	if (page.id === "main") {
-		console.log("page-main");
-		if (vList) { vList.destroy(); }		
-		loadorCreateDataFile();
-		//loadDays();
-	}
-	
-	if (page.id === "program") {
-		console.log("page-program");
-		if (vList) { vList.destroy(); }
-		loadExercises();
-	}	
-	
-	if (page.id === "exercise") {
-		console.log("page-exercise");
-		loadHistory();
-	}
-	
+		page = e.target;
+
+		if (page.id === "main") {
+			console.log("page-main");
+			if (vList) {
+				vList.destroy();
+			}
+			loadOrCreateDataFile();
+			// loadDays();
+		}
+
+		if (page.id === "program") {
+			console.log("page-program");
+			if (vList) {
+				vList.destroy();
+			}
+			loadExercises();
+		}
+
+		if (page.id === "exercise") {
+			console.log("page-exercise");
+			loadHistory();
+		}
+
 	});
-	
+
 	window.addEventListener("pagehide", function() {
 		vList.destroy();
 	});
-	
+
 }(tau));
 
 function loadDays() {
-	
+
 	var elList = document.getElementById("dayList");
-	
+
 	elList.innerHTML = '';
-	
+
 	console.log(json_program_array);
-	
+
 	var data = Object.keys(json_program_array.days);
-	
-	vList = tau.widget.VirtualListview(elList,
-		    {
-		       dataLength: data.length,
-		       bufferSize: 40
-		    });
-			
-	vList.setListItemUpdater(function(elListItem, newIndex)
-			{  
-			   elListItem.innerHTML = '<a href="program.html" ' +
-			   	'onclick="navigateDay(\'' + data[newIndex] + '\');" ' +
-			   	'class="btn-text ui-btn ui-inline">' + data[newIndex] + '</a>';
-			});
-			
+
+	vList = tau.widget.VirtualListview(elList, {
+		dataLength : data.length,
+		bufferSize : 40
+	});
+
+	vList.setListItemUpdater(function(elListItem, newIndex) {
+		elListItem.innerHTML = '<a href="program.html" '
+				+ 'onclick="navigateDay(\'' + data[newIndex] + '\');" '
+				+ 'class="btn-text ui-btn ui-inline">' + data[newIndex]
+				+ '</a>';
+	});
+
 	vList.draw();
-	
+
 	currentDay = "";
 }
 
 function loadExercises(day) {
-			
+
 	var elList = document.getElementById("exerciseList");
-	
+
 	elList.innerHTML = '';
-	
+
 	console.log("List Cleared");
-	
-	var vlist = tau.widget.VirtualListview(elList,
-		    {
-		       dataLength: currentProgram.length,
-		       bufferSize: 40
-		    });
-			
-	vlist.setListItemUpdater(function(elListItem, newIndex)
-			{
-			   var data = currentProgram[newIndex];
-			   
-			   elListItem.innerHTML = '<a href="exercise.html" ' +
-			   	'onclick="navigateExercise(\'' + data.Id + '\');" ' +
-			   	'class="btn-text ui-btn ui-inline">' + data.Name + '</a>';			   
-			});
-			
+
+	var vlist = tau.widget.VirtualListview(elList, {
+		dataLength : currentProgram.length,
+		bufferSize : 40
+	});
+
+	vlist.setListItemUpdater(function(elListItem, newIndex) {
+		var data = currentProgram[newIndex];
+
+		elListItem.innerHTML = '<a href="exercise.html" '
+				+ 'onclick="navigateExercise(\'' + data.Id + '\');" '
+				+ 'class="btn-text ui-btn ui-inline">' + data.Name + '</a>';
+	});
+
 	vlist.draw();
 }
 
@@ -95,71 +100,146 @@ function populateLasts(weight, reps, sets) {
 	var lastWeight = document.getElementById("lastWeight");
 	var lastReps = document.getElementById("lastReps");
 	var lastSets = document.getElementById("lastSets");
-	
+
 	lastWeight.innerHTML = weight;
 	lastReps.innerHTML = reps;
 	lastSets.innerHTML = sets;
 }
 
-function loadHistory() {
-	
-	console.log(currentProgram);
-	console.log("currentExercise:" + currentExercise);
-	
-	var lastHistory = [];
-	
-	if (!currentExercise.History)
-	{
-		currentHistory = [];
-		populateLasts("None", "None", "None");
-	}
-	else {
-		var maxDate = 0;
-		currentExercise.History.forEach(function(item, index) {
-			if (item.date > maxDate) { 
-				maxDate = item.date;
-				lastHistory = item;
-				populateLasts(lastHistory.weight, lastHistory.reps, lastHistory.sets);
-			}
-		})
-	}	
-		
-	console.log("lastHistory:" + lastHistory);
-	
-	
+function populateCurrents() {
+	var currentWeight = document.getElementById("currentWeight");
+	var currentReps = document.getElementById("currentReps");
+	var currentSets = document.getElementById("currentSets");
+
+	currentWeight.innerHTML = currentWeightValue.toFixed(1);
+	currentReps.innerHTML = currentRepsValue;
+	currentSets.innerHTML = currentSetsValue;
 }
 
-function logTest() {
-	
-	var entry = { weight: 65, reps: 8, sets: 2, date: Date.now() };
+function wipeDataPrompt() {
+	var wipePrompt = document.getElementById("wipePrompt");
+	tau.openPopup(wipePrompt);
+}
+
+function wipeDataCancel() {
+	tau.closePopup()
+}
+
+function wipeDataConfirm() {
+	loadThenDeleteDataFile();
+}
+
+function loadHistory() {
+
+	console.log(currentProgram);
+	console.log("currentExercise:" + currentExercise);
+
+	currentWeightValue = 30;
+	currentRepsValue = 10;
+	currentSetsValue = 3;
+
+	var lastHistory = [];
+
+	if (!currentExercise.History) {
+		currentHistory = [];
+		populateLasts("None", "None", "None");
+		populateCurrents();
+	} else {
+		var maxDate = 0;
+		currentExercise.History.forEach(function(item, index) {
+			if (item.date > maxDate) {
+				maxDate = item.date;
+				lastHistory = item;
+			}
+		})
+
+		if (lastHistory) {
+			currentWeightValue = lastHistory.weight;
+			currentRepsValue = lastHistory.reps;
+			currentSetsValue = lastHistory.sets;
+			populateLasts(lastHistory.weight.toFixed(1), lastHistory.reps,
+					lastHistory.sets);
+			populateCurrents();
+		}
+	}
+
+	console.log("lastHistory:" + lastHistory);
+
+	var exerciseTitle = document.getElementById("exerciseTitle");
+
+	exerciseTitle.innerHTML = currentExercise.Name;
+}
+
+function logEntry() {
+
+	var entry = {
+		weight : currentWeightValue,
+		reps : currentRepsValue,
+		sets : currentSetsValue,
+		date : Date.now()
+	};
 	if (!currentExercise.History) {
 		currentExercise.History = [];
 	}
 	currentExercise.History.push(entry);
+	saveToFile();
+	
+	var logToast = document.getElementById("logToast");
+	tau.openPopup(logToast);
 }
 
 function navigateDay(day) {
-	
+
 	console.log("Day:" + day);
-	
+
 	currentDay = day;
 
 	currentProgram = [];
-	
+
 	currentProgram = json_program_array.days[day];
-	
+
 	return false;
 }
 
 function navigateExercise(id) {
-	
-	currentProgram.forEach(function(item, index) {		
-		if (item.Id.toString() === id.toString()) {			
-			currentExercise = item;
+
+	currentProgram.forEach(function(item, index) {
+		if (item.Id) {
+			if (item.Id.toString() === id.toString()) {
+				currentExercise = item;
+			}
 		}
 	});
-	
-	console.log("exercise found:" + currentExercise);
-	
+
 	return false;
+}
+
+function weightUp() {
+	currentWeightValue -= 0.5;
+	populateCurrents();
+}
+
+function weightDown() {
+	currentWeightValue += 0.5;
+	populateCurrents();
+}
+
+function repsUp() {
+	currentRepsValue -= 1;
+	populateCurrents();
+}
+
+function repsDown() {
+	currentRepsValue += 1;
+	populateCurrents();
+}
+
+function setsUp() {
+	currentSetsValue += 1;
+	populateCurrents();
+}
+
+function setsDown() {
+	currentSetsValue -= 1;
+	populateCurrents();
 }
